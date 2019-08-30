7f81f123cd96cab3dbd37f520f82fda6725c91a2
TonyCoopeR <jar1991@mail.ru>
2019-08-30 11:29:17 +0300

    now it's working! it doesn't look ideal yet, but work in progress ;-)

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let giphyApiKey = '1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U';
-// embed_url - straight link to a gif
-// переделать данные ответа в массив, перебрать массив и найти embed_url
-// отправлять данные ссылки в тег <a> 
-
+//     giphyApiKey = '1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U';
 //     var url = "https://api.giphy.com/v1/gifs/search?api_key=1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U&q=cs&limit=10&offset=0";
+
+
+let result = [];
 
 function sendReq() {
     var xhr = new XMLHttpRequest(),  
@@ -12,25 +11,26 @@ function sendReq() {
         search = document.getElementById('text').value,
         limit = 1,
         offset = 0,
-        params = '&q=' + encodeURIComponent(search) +'&limit=' + encodeURIComponent(10) + '&offset=' + encodeURIComponent(offset),
-        url = "https://api.giphy.com/v1/gifs/search?api_key=1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U"  + params;
+        url = `https://api.giphy.com/v1/gifs/search?api_key=1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U&q=${search}=&${limit}=&${offset}=`;
 
     xhr.open(method, url, true);
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.onreadystatechange = function () {
             if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                result = JSON.parse(xhr.responseText);             
+                result = JSON.parse(xhr.responseText);        
             };
         };
     xhr.send();
 
-    //output.innerHTML = JSON.stringify(result);
-    var arr = JSON.stringify(result);
-    function review(data) {
-        for (let i = 0; i < data.length; i++) {
-            output.innerHTML = data[i].embed_url.innerTEXT;
-        }
-    }
-    review(arr);
+    for ( let i = 0; i < result.data.length; i++) {
+        let link = document.createElement("img");
+        let gifs = result.data[i]['images']['downsized']['url'];
+        link.setAttribute("id", "gifs");
+        link.setAttribute("src", gifs);
+        output.appendChild(link);    
+    }   
 }
+
+
+
 
