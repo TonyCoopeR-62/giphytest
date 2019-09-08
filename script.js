92aa78afe4f1e6a5d124010115a52e11e231e78c
TonyCooper-62 <jar1991@mail.ru>
2019-09-08 13:27:52 +0300

    change and fix ajax function, its work well now. But I want to fix html and css code too, very soon

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-//     giphyApiKey = '1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U';
-
-let result = [];
-let visible = document.getElementById('output');
-let arrow = document.getElementById('arrow');
-
+// giphyApiKey = '1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U';
+// keypressed enter Добавить
+// добавить в форму кнопку reset и сбрасывать гифки
 function sendReq() {
-    let xhr = new XMLHttpRequest(),  
-        output = document.getElementById('output'),
-        method = "GET",
-        search = document.getElementById('text').value,
-        limit = document.getElementById('select').value,
-        offset = 0,
-        url = `https://api.giphy.com/v1/gifs/search?api_key=1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U&q=${search}=&limit=${limit}=&offset=${offset}`;
-        xhr.open(method, url, true);
-        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        xhr.onreadystatechange = function () {
-                if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                    result = JSON.parse(xhr.responseText);        
-                };
-            };
-        xhr.send();   
-        for ( let i = 0; i < result.data.length; i++) {
-            let link = document.createElement("img");
-            let gifs = result.data[i]['images']['downsized']['url'];
-            link.setAttribute("id", "gifs");
-            link.setAttribute("src", gifs);
-            output.appendChild(link);   
-        }
-        arrow.style.display = 'inline';
-        visible.style.display = 'block';
+  let output = document.getElementById('output'),
+      limit = document.getElementById('select').value,
+      offset = 0,
+      search = document.getElementById('text').value,
+      url = `https://api.giphy.com/v1/gifs/search?api_key=1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U&q=${search}&limit=${limit}&offset=${offset}`;
+    axios.get(url)
+    .then(function (response) {
+      for (let i = 0; i < response.data['data'].length; i++) {
+        let gifs = response.data['data'][i]['images']['downsized']['url'];
+        let link = document.createElement('img');
+        output.appendChild(link);
+        link.setAttribute('id', 'gifs');
+        link.setAttribute('src', gifs);
+      };
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
-
-
-
-
-
