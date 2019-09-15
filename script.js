4da1fe0032ca96ecd1ff03d395dab9078bc4a98f
TonyCooper-62 <jar1991@mail.ru>
2019-09-15 11:15:40 +0300

    align and variables fix

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // giphyApiKey = '1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U';
-// keypressed enter Добавить
-// добавить в форму кнопку reset и сбрасывать гифки
+
 function sendReq() {
-  let output = document.getElementById('output'),
+  const output = document.getElementById('output'),
       limit = document.getElementById('select').value,
       offset = 0,
       search = document.getElementById('text').value,
       url = `https://api.giphy.com/v1/gifs/search?api_key=1tqwMywkT5HjTrmdg8aF7mw5ZN2h0p2U&q=${search}&limit=${limit}&offset=${offset}`;
+    // clean form
+      output.innerHTML = ""; 
+
+    // request
     axios.get(url)
     .then(function (response) {
       for (let i = 0; i < response.data['data'].length; i++) {
-        let gifs = response.data['data'][i]['images']['downsized']['url'];
-        let link = document.createElement('img');
+        const gifs = response.data['data'][i]['images']['downsized']['url'];
+        const link = document.createElement('img');
         output.appendChild(link);
         link.setAttribute('id', 'gifs');
         link.setAttribute('src', gifs);
